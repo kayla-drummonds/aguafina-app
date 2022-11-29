d5c53b97371ab4a41bb64550c1c356bf4240b650
Michaela Drummonds <mickeysymone@gmail.com>
2022-11-29 18:42:31 -0500

    list all orders not paginated

--- src/main/java/com/michaeladrummonds/aguafina/controller/OrderController.java
+++ src/main/java/com/michaeladrummonds/aguafina/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.michaeladrummonds.aguafina.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.michaeladrummonds.aguafina.models.Customer;
 import com.michaeladrummonds.aguafina.models.Employee;
@@ -41,26 +39,7 @@ public class OrderController {
         Employee employee = employeeService.getEmployeeByEmail(username);
         model.addAttribute("employee", employee);
 
-        return findPaginated(1, "id", "asc", model, authentication);
-    }
-
-    @GetMapping("/orders/{pageNo}")
-    public String findPaginated(@PathVariable(value = "pageNo") int pageNo, @RequestParam("sortField") String sortField,
-            @RequestParam("sortDir") String sortDir, Model model, Authentication authentication) {
-
-        String username = authentication.getName();
-        Employee employee = employeeService.getEmployeeByEmail(username);
-        model.addAttribute("employee", employee);
-
-        int pageSize = 10;
-        Page<Order> page = orderService.findPaginated(pageNo, pageSize, sortField, sortDir);
-        List<Order> orders = page.getContent();
-        model.addAttribute("currentPage", pageNo);
-        model.addAttribute("totalPages", page.getTotalPages());
-        model.addAttribute("totalItems", page.getTotalElements());
-        model.addAttribute("sortField", sortField);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+        List<Order> orders = orderService.getAllOrders();
         model.addAttribute("orders", orders);
         return "orders";
     }
