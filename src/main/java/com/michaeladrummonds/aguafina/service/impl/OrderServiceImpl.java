ed593b7b9ab65f22840f313b95ed3ffe14452717
Michaela Drummonds <mickeysymone@gmail.com>
2022-11-29 18:39:56 -0500

    deleted pagination method

--- src/main/java/com/michaeladrummonds/aguafina/service/impl/OrderServiceImpl.java
+++ src/main/java/com/michaeladrummonds/aguafina/service/impl/OrderServiceImpl.java
@@ -3,11 +3,6 @@ package com.michaeladrummonds.aguafina.service.impl;
 import java.util.List;
 
 import javax.transaction.Transactional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.michaeladrummonds.aguafina.models.Customer;
@@ -59,14 +54,6 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public List<Order> getOrderByEmployeeId(Employee employee, Integer id) {
         return orderRepository.findByEmployeeId(employee, id);
-    }
-
-    public Page<Order> findPaginated(int pageNo, int pageSize, String sortField, String sortDir) {
-        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending()
-                : Sort.by(sortField).descending();
-
-        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
-        return this.orderRepository.findAll(pageable);
     }
 
 }
