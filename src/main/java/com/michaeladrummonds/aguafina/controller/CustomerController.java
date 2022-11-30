bd90416d7b2ec8c3e42d83b7a24409853b9d7fd8
Michaela Drummonds <mickeysymone@gmail.com>
2022-11-29 19:01:21 -0500

    removed pagination

--- src/main/java/com/michaeladrummonds/aguafina/controller/CustomerController.java
+++ src/main/java/com/michaeladrummonds/aguafina/controller/CustomerController.java
@@ -3,9 +3,6 @@ package com.michaeladrummonds.aguafina.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,32 +31,22 @@ public class CustomerController {
     // displays all customers
     @GetMapping({ "/customers", "/customers/page/{pageNo}" })
     public String listCustomers(Model model,
-            @RequestParam(value = "keyword", required = false) String keyword,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "12") int size, Authentication authentication) {
+            @RequestParam(value = "keyword", required = false) String keyword, Authentication authentication) {
 
         String username = authentication.getName();
         Employee employee = employeeService.getEmployeeByEmail(username);
 
         model.addAttribute("employee", employee);
 
-        Pageable paging = PageRequest.of(page, size);
-        Page<Customer> pageCustomers = customerService.getAllCustomersPageable(paging);
-
         if (keyword != null) {
             List<Customer> customers = customerService.getCustomerByKeyword(keyword);
             model.addAttribute("customers", customers);
             model.addAttribute("keyword", keyword);
         } else {
-            pageCustomers = customerService.getAllCustomersPageable(paging);
-            List<Customer> customers = pageCustomers.getContent();
+            List<Customer> customers = customerService.getAllCustomers();
             model.addAttribute("customers", customers);
             model.addAttribute("keyword", keyword);
         }
-
-        model.addAttribute("currentPage", pageCustomers.getNumber());
-        model.addAttribute("totalItems", pageCustomers.getTotalElements());
-        model.addAttribute("totalPages", pageCustomers.getTotalPages());
         return "customers";
     }
 
