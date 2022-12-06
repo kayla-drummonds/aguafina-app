43eb960a771b5add054086127c9abe65e2fedbd7
Michaela Drummonds <mickeysymone@gmail.com>
2022-12-06 11:48:24 -0500

    deleted UserNotAuthorizedException

--- src/main/java/com/michaeladrummonds/aguafina/exceptions/UserNotAuthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.michaeladrummonds.aguafina.exceptions;
-
-public class UserNotAuthorizedException extends Exception {
-
-    public UserNotAuthorizedException() {
-        super("User not authorized to access this page.");
-    }
-}
