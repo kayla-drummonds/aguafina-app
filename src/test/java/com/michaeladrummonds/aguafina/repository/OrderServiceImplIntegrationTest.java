e7a204e35e736d6b72cb81cb0aa5b08358b4bfa5
Michaela Drummonds <mickeysymone@gmail.com>
2022-12-11 15:02:29 -0500

    corrected sample order data

--- src/test/java/com/michaeladrummonds/aguafina/repository/OrderServiceImplIntegrationTest.java
+++ src/test/java/com/michaeladrummonds/aguafina/repository/OrderServiceImplIntegrationTest.java
@@ -51,8 +51,8 @@ public class OrderServiceImplIntegrationTest {
 
         User u3 = new User();
         u3.setId(3);
-        u3.setEmail("rdean2@hplussport.com");
-        u3.setPassword("rdean2");
+        u3.setEmail("egarcia1@hplussport.com");
+        u3.setPassword("egarcia1");
         userRepository.save(u3);
 
         User u4 = new User();
@@ -66,9 +66,8 @@ public class OrderServiceImplIntegrationTest {
                 u1);
         employeeRepository.save(e1);
 
-        Employee e2 = new Employee(3, "Richard", "Dean", "rdean2@hplussport.com", "2482 1st road", "Houston",
-                "TX", "77228", null,
-                u3);
+        Employee e2 = new Employee(2, "Emily", "Garcia", "egarcia1@hplussport.com", "97 Vidon Alley", "Manchester",
+                "NH", "31050", null, u3);
         employeeRepository.save(e2);
 
         Customer c1 = new Customer(1, "Carol", "Shaw", "cshaw0@mlb.com", "206-804-8771", "8157 Longview Court",
@@ -99,21 +98,22 @@ public class OrderServiceImplIntegrationTest {
     }
 
     @ParameterizedTest
-    @CsvSource({ "2,2022-11-13 00:00:00,MWCRA32,24,1,2,6" })
+    @CsvSource({ "1, 2022-12-02 16:07:59, MWBLU32, 3, 12, 1, 1" })
     public void testSumTotalByCustomer(ArgumentsAccessor accessor) throws ArgumentAccessException, ParseException {
         Order o = new Order();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         o.setId(accessor.getInteger(0));
         o.setCreationDate(formatter.parse(accessor.getString(1)));
         o.setProduct(accessor.getString(2));
-        o.setTotal(accessor.getDouble(3));
+        o.setQuantity(accessor.getInteger(3));
+        o.setTotal(accessor.getDouble(4));
         o.setCustomer(customerRepository.findByEmail("cshaw0@mlb.com"));
         o.setEmployee(employeeRepository.findByEmail("jpowell0@hplussport.com"));
-        o.setQuantity(accessor.getInteger(6));
+
         orderRepository.save(o);
 
         Double totalFound = orderRepository.sumTotalByCustomer(o.getCustomer(), 1);
-        assertEquals(24, totalFound);
+        assertEquals(12, totalFound);
     }
 
     @ParameterizedTest
@@ -124,9 +124,9 @@ public class OrderServiceImplIntegrationTest {
         o.setId(accessor.getInteger(0));
         o.setCreationDate(formatter.parse(accessor.getString(1)));
         o.setProduct(accessor.getString(2));
-        o.setTotal(accessor.getDouble(3));
-        o.setCustomer(customerRepository.findByEmail("cshaw0@mlb.com"));
-        o.setEmployee(employeeRepository.findByEmail("jpowell0@hplussport.com"));
+        o.setTotal(accessor.getDouble(5));
+        o.setCustomer(customerRepository.findByEmail("ecarr1@oracle.com"));
+        o.setEmployee(employeeRepository.findByEmail("egarcia1@hplussport.com"));
         o.setQuantity(accessor.getInteger(6));
         orderRepository.save(o);
 
@@ -135,20 +135,20 @@ public class OrderServiceImplIntegrationTest {
     }
 
     @ParameterizedTest
-    @CsvSource({ "1,'2022-12-02 16:07:59','MWBLU32',1,1,12,3" })
+    @CsvSource({ "2,'2022-12-02 16:08:16','MWCRA32',2,2,16,4" })
     public void testFindByEmployeeId(ArgumentsAccessor accessor) throws ArgumentAccessException, ParseException {
         Order o = new Order();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         o.setId(accessor.getInteger(0));
         o.setCreationDate(formatter.parse(accessor.getString(1)));
         o.setProduct(accessor.getString(2));
-        o.setTotal(accessor.getDouble(3));
-        o.setCustomer(customerRepository.findByEmail("ecarr1@mlb.com"));
-        o.setEmployee(employeeRepository.findByEmail("rdean2@hplussport.com"));
+        o.setTotal(accessor.getDouble(5));
+        o.setCustomer(customerRepository.findByEmail("ecarr1@oracle.com"));
+        o.setEmployee(employeeRepository.findByEmail("egarcia1@hplussport.com"));
         o.setQuantity(accessor.getInteger(6));
         orderRepository.save(o);
 
-        List<Order> ordersFound = orderRepository.findByEmployeeId(o.getEmployee(), 3);
+        List<Order> ordersFound = orderRepository.findByEmployeeId(o.getEmployee(), 2);
         assertEquals(1, ordersFound.size());
     }
 
@@ -168,7 +168,7 @@ public class OrderServiceImplIntegrationTest {
         orderRepository.save(o);
 
         orderRepository.deleteById(o.getId());
-        List<Order> orders = orderRepository.findByCustomerId(o.getCustomer(), 2);
-        assertFalse(orders.contains(o));
+        List<Order> orders = orderRepository.findByCustomerId(o.getCustomer(), 1);
+        assertEquals(0, orders.size());
     }
 }
